41f504ed080dcc1b9602b4f71ecde098cbe42af4
lubigsis <lucre.baluc@gmail.com>
2022-03-04 14:32:38 -0300

    edite HTML con los id asi poder referenciarlos en Javascript, ademas en el main.js hice una funcion para que al hacer click en algun boton, se escriba el valor en el display de la calculadora. Tambien cree una variable cifra con una cadena vacia para asi poder elegir numeros de mas cifras y que no se borren. Finalmente hice un script que quedara al final del documento para que al actualizar la pagina web, el display muestre 0 y no quede vacio

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
-console.log(funcionando);
+/*funcion generica que recibe el valor del boton que se clickea, y se muestra en el display xq cada boton le pasa por parametro en el HTML el numero correspondiente */
+/*cifra, q en principio es una cadena vacia, va adquieriendo los valores q se clickean, y los va concatenendo con los otros nros que se aprietan*/
 
+let cifra= "";
+function display_numeros(numero){
+
+    document.getElementById("displayJS").value= cifra + numero;
+    cifra= document.getElementById("displayJS").value;
+}
+
+
+
+
+/**dejar el siguiente codigo al final para que el display se sobreescriba con 0 cada vez que se actualice la pagina*/
+document.getElementById("displayJS").value=0;
