ec0892968d26434e703020d13e19f0cb2e98c319
lubigsis <lucre.baluc@gmail.com>
2023-02-25 18:03:41 -0300

    Edité CSS y empecé otra vez el JavaScript

--- main.js
+++ main.js
@@ -1,81 +1,15 @@
-/*funcion generica que recibe el valor del boton que se clickea, y se muestra en el display xq cada boton le pasa por parametro en el HTML el numero correspondiente */
-/*cifra, q en principio es una cadena vacia, va adquieriendo los valores q se clickean, y los va concatenendo con los otros nros que se aprietany mostrando en el display*/
+window.addEventListener('load', () => { //p/escuchar q' se carga el documento HTMl.
+    //-------------referencio variables p/buscar el display y los botones (se guargarn en un HTML collection y ds lo convierto a array).
+    let display = document.getElementById('displayJS');
+    let keypadButtons = document.getElementsByClassName('btn');
+//-----------acá creo otra variable p/convertir el HTMLCollection a Array
+    let keypadButtonsArray = Array.from(keypadButtons);
 
-let cifra= "";
-let acumulado= 0;
-let primeraOperacion= true;
-let sumar= false;
-let restar= false;
-
-function display_numeros(numero){
-
-    document.getElementById("displayJS").value= cifra + numero;
-    cifra= document.getElementById("displayJS").value;
-}
-
-/**para que vaya apareciendo de a una vez los numeros al presionar el boton de sumar. La variable cifra lo va acumulando */
-
- //primero almaceno y luego reseteo la variable cifra
-
-function suma(){  
-    
-    if (restar){
-        acumulado= acumulado - parseInt(cifra);
-        document.getElementById("displayJS").value= acumulado;
-    }else{
-        acumulado= acumulado + parseInt(cifra);
-        document.getElementById("displayJS").value= acumulado;
-}
-    cifra="";
-    sumar=true;
-    restar= false;
-}
-
-/**funcion resta; si lo ultimo que hiciste fue una resta, sumale a acumulado lo que hay en el display */
-
-function resta(){
-
-    if(primeraOperacion==false){
-
-    if (sumar){
-        acumulado=acumulado + parseInt(cifra);
-        document.getElementById("displayJS").value= acumulado;
-
-    }else{
-        acumulado = acumulado - parseInt(cifra);
-        document.getElementById("displayJS").value= acumulado;
-    }
-}else{
-    acumulado= parseInt(cifra);
-    primeraOperacion= false;
-}
-        cifra="";
-        sumar= false;
-        restar= true;
-    }
-
-/**detectar que operaciones se quiere realizar mediante el if.  */
-//si sumar es true => if (sumar) es igual a: if (sumar==true)
-//luego de hacer la operacion correspondiente, en acumulado almaceno lo que hay en el display.
-//tmb reseteo cifra con 0 asi el nro que aparece en pantalla no interfiere con el cambio de operacion
-
-function resultado(){
-    if (sumar){
-
-        document.getElementById("displayJS").value= acumulado + parseInt(cifra);
-
-    }else if(restar){
-
-        document.getElementById("displayJS").value= acumulado - parseInt(cifra);
-    }
-
-    acumulado= parseInt(document.getElementById("displayJS").value);
-    cifra= 0;
-}
-
-
-
-
-
-/**dejar el siguiente codigo al final para que el display se sobreescriba con 0 cada vez que se actualice la pagina*/
-document.getElementById("displayJS").value=0;
+    //----Ahora sí puedo iterr en este nuevo Array creado
+    keypadButtonsArray.forEach( (button) =>{
+        //---a c/boton se le agrega un listener con el forEach para escuchar cuando el user hace click y lo imprime.
+        button.addEventListener('click', ()=>{
+            console.log(button.innerHTML);
+        })
+    })
+});
